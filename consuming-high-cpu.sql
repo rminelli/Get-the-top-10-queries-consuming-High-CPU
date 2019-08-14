d0d3a2f93caf6009b13de92ae5e47503bb115cb3
Roberto Minelli <rminelli@gmail.com>
2019-08-14 11:00:45 -0300

    First Commit

---
+++ consuming-high-cpu.sql
@@ -0,0 +1,63 @@
+/*
+	Get the top 10 queries consuming High CPU 
+	T-SQL - Microsoft SQL Server
+
+	Roberto Minelli
+	01/2019
+*/
+
+
+
+SELECT s.session_id,
+
+r.status,
+
+r.blocking_session_id 'Blocked by',
+
+r.wait_type,
+
+wait_resource,
+
+r.wait_time / (1000 * 60) 'Wait in Minutes',
+
+r.cpu_time,
+
+r.logical_reads,
+
+r.reads,
+
+r.writes,
+
+r.total_elapsed_time / (1000 * 60) 'Elaps in Minutes',
+
+Substring(st.TEXT,(r.statement_start_offset / 2) + 1,
+
+((CASE r.statement_end_offset 
+	WHEN -1 THEN Datalength(st.TEXT)
+ELSE r.statement_end_offset
+
+END - r.statement_start_offset) / 2) + 1) AS statement_text,
+
+Coalesce(Quotename(Db_name(st.dbid)) + N'.' + Quotename(Object_schema_name(st.objectid, st.dbid)) + N'.' +
+Quotename(Object_name(st.objectid, st.dbid)), '') AS command_text,
+r.command,
+
+s.login_name,
+
+s.host_name,
+
+s.program_name,
+
+s.last_request_end_time,
+
+s.login_time,
+
+r.open_transaction_count
+
+FROM sys.dm_exec_sessions AS s
+
+JOIN sys.dm_exec_requests AS r ON r.session_id = s.session_id
+
+CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) AS st WHERE r.session_id != @@SPID
+
+ORDER BY r.cpu_time desc
